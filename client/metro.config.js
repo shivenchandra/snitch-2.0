d257d1262ed34a7dabb21bdc7f97451342a53155
Shiven Chandra <137705277+shivenchandra@users.noreply.github.com>
2026-05-02 02:08:24 +0530

    fix: disable reactCompiler to fix EAS build crash

--- client/metro.config.js
+++ client/metro.config.js
@@ -2,4 +2,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for Metro module resolution crash on EAS build servers (Linux)
+// with react-native-reanimated 4.x
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
